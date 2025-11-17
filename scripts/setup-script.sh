03e2316786994a1af6f63cbfc6635675300b90c6
phate <152439270+phatetr0n@users.noreply.github.com>
2025-11-16 21:25:25 -0500

    full rewrite of setup script now that I know more about bash

--- scripts/setup-script.sh
+++ scripts/setup-script.sh
@@ -2,77 +2,131 @@
 # Complete installer and configuration script for Niri.
 # Written for Fedora
 
-# Repos
-sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm # fusion free
-sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm # fusion nonfree
-sudo dnf -y copr enable yalter/niri # niri
-sudo dnf -y copr enable solopasha/hyprland # hypr utilities
-sudo dnf -y copr enable alternateved/eza # eza
+cat <<"EOF"
+       _           _               _      _            _
+ _ __ | |__   __ _| |_ ___   _ __ (_)_ __(_)  ___  ___| |_ _   _ _ __
+| '_ \| '_ \ / _` | __/ _ \ | '_ \| | '__| | / __|/ _ \ __| | | | '_ \
+| |_) | | | | (_| | ||  __/ | | | | | |  | | \__ \  __/ |_| |_| | |_) |
+| .__/|_| |_|\__,_|\__\___| |_| |_|_|_|  |_| |___/\___|\__|\__,_| .__/
+|_|                                                             |_|
 
-# Tools
-sudo dnf -y install libnotify qt5-qtwayland qt6-qtwayland python-pip python3-pip nm-connection-editor network-manager-applet fuse ImageMagick NetworkManager-tui flatpak fastfetch figlet xclip
-# enable flathub repo to flatpak
+EOF
+
+# repos
+repo=(
+   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+)
+
+# copr repos
+copr=(
+   yalter/niri
+   solopasha/hyprland
+   alternateved/eza
+)
+
+# general purpose tools or libraries used by other stuff
+tools=(
+   libnotify
+   qt5-qtwayland
+   qt6-qtwayland
+   python-pip
+   python3-pip
+   nm-connection-editor
+   network-manager-applet
+   fuse
+   ImageMagick
+   NetworkManager-tui
+   flatpak
+   fastfetch
+   figlet
+   xclip
+   git
+)
+
+# stuff core to the customized dwm
+core=(
+   niri
+   zsh
+   eza
+   hyprlock
+   waypaper
+   waybar
+   wlogout
+)
+
+# extra utilities
+extra=(
+   thunderbird
+   kate
+)
+
+# flatpaks
+flatpaks=(
+   com.discordapp.Discord
+   tv.plex.PlexDesktop
+   com.spotify.Client
+)
+
+# enable copr repositories
+echo "Enabling COPR repos and other third party repositories..."
+for acopr in "${copr[@]}"; do
+   sudo dnf -y copr enable "$acopr"
+done
+
+# install repos
+for arepo in "${repo[@]}"; do
+   sudo dnf -y install "$arepo"
+done
+
+# install core, tools, and extra packages
+echo "Installing packages..."
+sudo dnf -y install "${core[@]}" "${tools[@]}" "${extra[@]}"
+
+# add flatpak repo and install flatpaks
+echo "Enabling flatpak repo and installing flatpaks..."
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+for aflatpaks in "${flatpaks[@]}"; do
+   flatpak install -y flathub "$aflatpaks"
+done
 
-# Niri DWM
-sudo dnf -y install niri
-
-# ZSH and utilities
-sudo dnf -y install zsh eza
-# oh-my-zsh
+# install oh-my-zsh then clone plugins
+echo "Installing oh-my-zsh and plugins..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && zsh -c './setup-script.sh'
-# clone oh-my-zsh plugins into their proper place
-sudo git clone https://github.com/chrissicool/zsh-256color ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-256color # zsh-256color
-sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions # zsh-autosuggestions
-sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting # zsh-syntax-highlighting
-sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k # oh-my-zsh p10k install
+git clone https://github.com/chrissicool/zsh-256color \
+  "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-256color"
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+  "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+  "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+  "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
-# Hypr apps
-sudo dnf -y install hyprlock
-
-# Waypaper and waybar
-sudo dnf -y install waypaper waybar
-
-# Thunderbird
-sudo dnf -y install thunderbird
-
-# Discord, Plex, Spotify (flatpaks)
-flatpak -y install flathub com.discordapp.Discord
-flatpak -y install flathub tv.plex.PlexDesktop
-flatpak -y install flathub com.spotify.Client
-
-# Wlogout
-sudo dnf -y install wlogout
-
-# Kate
-sudo dnf -y install kate
-
-# pywal and pywalfox
-# pywal16
+# pywal16 and pywalfox
+echo "Installing python tools..."
 pip3 install pywal16
-# pywalfox
 pip install pywalfox
 pywalfox install
 
-# Fonts (incl with dotfiles dl)
-# create fonts and sorting directories
-mkdir $HOME/.fonts
-mkdir $HOME/.fonts/m
-# copy fonts from .extra and place in new font directories
-cp $dfextra/MesloLGS*.ttf $HOME/.fonts/m
-# create .font.config file
-echo "<?xml version=\"1.0\"?><!DOCTYPE fontconfig SYSTEM \"fonts.dtd\">
+# create font directory for fonts I use and install
+echo "Creating font directory and config..."
+mkdir -p "$HOME/.fonts/m"
+cp "$HOME/dotfiles/.extra"/MesloLGS*.ttf "$HOME/.fonts/m"
+cat > "$HOME/.fonts.conf" <<EOF
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
 <fontconfig>
- <dir>~/.fonts</dir>
-</fontconfig>" > $HOME/.fonts.config
+  <dir>$HOME/.fonts</dir>
+</fontconfig>
+EOF
 
-# Assign dark mode for gtk
+# assign dark mode for gtk
+echo "Assigning dark mode for gtk..."
 dconf write /org/gnome/desktop/interface/color-scheme '"prefer-dark"'
 
 # prevent wal lockup by assigning bg
-cp $HOME/dotfiles/.extra/wallpaper.jpg /var/tmp/wp.png
+echo "Copying background..."
+cp "$HOME/dotfiles/.extra/wallpaper.jpg" /var/tmp/wp.png
 wal -i /var/tmp/wp.png
 
-
-
-
+echo "Done!"
